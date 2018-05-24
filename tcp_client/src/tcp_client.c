962b679de082cecf6a778f8d9147a333142318fa
mkhasan <hasan@kaist.ac.kr>
2018-05-23 20:23:21 -0400

    Initial commit

---
+++ tcp_client/src/tcp_client.c
@@ -0,0 +1,71 @@
+#include<stdio.h> //printf
+#include<string.h>    //strlen
+#include<sys/socket.h>    //socket
+#include<arpa/inet.h> //inet_addr
+ 
+int main(int argc , char *argv[])
+{
+    int sock;
+    struct sockaddr_in server;
+    char message[1000] , server_reply[2000];
+     
+    if(argc < 2)
+    	return -1;
+    //Create socket
+    sock = socket(AF_INET , SOCK_STREAM , 0);
+    if (sock == -1)
+    {
+        printf("Could not create socket");
+    }
+    puts("Socket created");
+     
+    server.sin_addr.s_addr = inet_addr(argv[1]);
+    server.sin_family = AF_INET;
+    server.sin_port = htons( 10002 );
+ 
+    //Connect to remote server
+    if (connect(sock , (struct sockaddr *)&server , sizeof(server)) < 0)
+    {
+        perror("connect failed. Error");
+        return 1;
+    }
+     
+    puts("Connected\n");
+     
+    //keep communicating with server
+    while(1)
+    {
+        printf("Enter message : ");
+        scanf("%s" , message);
+         
+        //Send some data
+        if( send(sock , message , strlen(message) , 0) < 0)
+        {
+            puts("Send failed");
+            return 1;
+        }
+         
+        //Receive a reply from the server
+        if( recv(sock , server_reply , 2000 , 0) < 0)
+        {
+            puts("recv failed");
+            break;
+        }
+         
+        printf("Server reply (%d):\n", strlen(server_reply));
+        int k;
+        for(k=0; k<2000 && server_reply[k]!=0; k++) {
+        	if(server_reply[k] < 'A') {
+        		printf(" %d", server_reply[k]);
+        	}
+        	else
+        		printf("%c", server_reply[k]);
+        }
+
+        printf("\n");
+        //puts(server_reply);
+    }
+     
+    close(sock);
+    return 0;
+}
